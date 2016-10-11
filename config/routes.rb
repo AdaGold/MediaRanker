ea466ed086bef26518d5637feb695a6027d20220
Alyssa Hursh <akhursh@gmail.com>
2016-10-11 14:16:07 -0700

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,48 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/destroy'
+
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/new'
+
+  get 'movies/create'
+
+  get 'movies/edit'
+
+  get 'movies/update'
+
+  get 'movies/destroy'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/edit'
+
+  get 'books/update'
+
+  get 'books/destroy'
+
+  get 'pages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
