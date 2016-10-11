86f117251cf8307aca45e119ca3cc2ba0c83e93a
laurenfb <laurenfriesbrink@gmail.com>
2016-10-11 13:54:59 -0700

    added ap and faker gems & seeded db

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# print things awesomely
+gem 'awesome_print'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -42,7 +45,7 @@ group :development do
   gem 'web-console', '~> 2.0'
   gem 'better_errors'
   gem 'binding_of_caller'
-
+  gem 'faker'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+15.times do
+  Album.create(name: Faker::Superhero.power,
+          creator: Faker::Pokemon.name,
+          description: Faker::ChuckNorris.fact,
+          votes: 0)
+end
+
+15.times do
+  Book.create(name: Faker::Book.title,
+          creator: Faker::Name.name_with_middle,
+          description: Faker::ChuckNorris.fact,
+          votes: 0)
+end
+
+15.times do
+  Movie.create(name: Faker::Space.nebula,
+          creator: Faker::StarWars.character,
+          description: Faker::Hipster.sentence,
+          votes: 0)
+end
