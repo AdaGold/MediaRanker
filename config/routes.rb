c0793c55c6a2bb31becaa16cad5b478cd266772f
laurenfb <laurenfriesbrink@gmail.com>
2016-10-11 13:41:32 -0700

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,48 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+
+  get 'albums/index'
+
+  get 'albums/show'
+
+  get 'albums/edit'
+
+  get 'albums/update'
+
+  get 'albums/new'
+
+  get 'albums/create'
+
+  get 'albums/destroy'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/edit'
+
+  get 'books/update'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/destroy'
+
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/edit'
+
+  get 'movies/update'
+
+  get 'movies/new'
+
+  get 'movies/create'
+
+  get 'movies/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
