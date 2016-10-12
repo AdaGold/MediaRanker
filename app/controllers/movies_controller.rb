a739f8d37f15d7eaf1a128858541cb92184eccfc
jm-rives <joanna.rives@gmail.com>
2016-10-12 13:13:05 -0700

    ADD index, new, create methods to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,43 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    # want all movies to display on welcome page
+    render "index"
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new
+    @task.name = params[:movie][:name]
+    @task.director = params[:movie][:director]
+    @task.description = params[:movie][:description]
+
+    redirect_to root_path
+  end
+
+
+  def edit
+
+
+  end
+
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+  def show
+
+  end
+
+
 end
