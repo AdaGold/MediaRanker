231cb5a95ad8eab95e0510861f531d0ee0ef203c
jm-rives <joanna.rives@gmail.com>
2016-10-13 08:05:59 -0700

    ADD forgotten rankings column to Books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013150200) do
+ActiveRecord::Schema.define(version: 20161013150524) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161013150200) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "rankings"
   end
 
   create_table "movies", force: :cascade do |t|
