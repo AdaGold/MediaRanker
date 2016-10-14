289e85592273012eb3c8c70b0e38932e211b8425
jm-rives <joanna.rives@gmail.com>
2016-10-14 07:45:40 -0700

    ADD rankings migration to books, albums, and movies.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013151345) do
+ActiveRecord::Schema.define(version: 20161014144419) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161013151345) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "ranking"
+    t.integer  "rankings"
   end
 
   create_table "votes", force: :cascade do |t|
