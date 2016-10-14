2b272205013fe8322ec25336005444011bbae02a
laurenfb <laurenfriesbrink@gmail.com>
2016-10-13 20:57:34 -0700

    tests for books controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,39 +1,114 @@
-# require 'test_helper'
-#
-# class BooksControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
-#
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
-#
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
-#
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
-#
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
-#
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
-#
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
-#
-# end
+require 'test_helper'
+
+class BooksControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show, {id: books(:in_the_woods)}
+    assert_response :success
+  end
+
+  test 'should not get show if resource DNE' do
+    params = {id: 14678}
+    assert_raises ArgumentError do
+      get :show, params
+    end
+
+    params = {id: "are you a cat?"}
+    assert_raises ArgumentError do
+      get :show, params
+    end
+
+    params = {id: 100.0001}
+    assert_raises ArgumentError do
+      get :show, params
+    end
+  end
+
+  test "should get edit" do
+    get :edit, {id: books(:dreams)}
+    assert_response :success
+  end
+
+  test 'should not get edit if resource DNE' do
+    params = {id: "cat"}
+    assert_raises ArgumentError do
+      get :edit, params
+    end
+
+    params = {id: 2434}
+    assert_raises ArgumentError do
+      get :edit, params
+    end
+
+    params = {id: 0.000023434}
+    assert_raises ArgumentError do
+      get :edit, params
+    end
+
+  end
+
+  test "should be able to update" do
+    params = {id: books(:dreams),
+            book: {name: books(:dreams)}}
+    put :update, params
+    assert_response :redirect
+  end
+
+  test 'should not be able to update something that does not exist' do
+    params = {id: 232546544}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+
+    params = {id: :hello_its_me}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+
+    params = {id: "adele"}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should be able to create" do
+    post :create, {book: {id: 1044,
+                  name: "ediff biography",
+                  creator: "christian brink",
+                  description: "my husband is an author",
+                  votes: 1000}}
+    assert_response :redirect
+  end
+
+  test "should be able to destroy" do
+    delete :destroy, {id: books(:in_the_woods)}
+    assert_response :redirect
+  end
+
+  test 'should not be able to destroy something that does not exist' do
+    params = {id: "barack obama"}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+
+    params = {id: 400.04}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+
+    params = {id: 309}
+    assert_raises ArgumentError do
+      put :update, params
+    end
+  end
+
+end
