ad41bc6f2902948e561c9690060639d31ccab4d0
jm-rives <joanna.rives@gmail.com>
2016-10-15 12:49:23 -0700

    remove ranking (singular) from movie table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014150448) do
+ActiveRecord::Schema.define(version: 20161015194721) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20161014150448) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "ranking"
     t.integer  "rankings"
   end
+
 end
