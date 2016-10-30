83656193821e2e1bf95ae6840b8febbb7fe34e86
jm-rives <joanna.rives@gmail.com>
2016-10-30 14:35:59 -0700

    ADD UPDATES to welcome page with sort by rank descending, change table to unordered list, update wlecome controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,9 @@ class WelcomeController < ApplicationController
   def index
     # localhost:3000 by itself without anything else in the url is the root (or '/') route and is always going to go to WelcomeController.index
 
-    @movies = Movie.all
-    @books = Book.all
-    @albums = Album.all
+    @first_10_movies = Movie.order(rankings: :desc).limit(10)
+    @first_10_books = Book.order(rankings: :desc).limit(10)
+    @first_10_albums = Album.order(rankings: :desc).limit(10)
 
     #so if you want books and albums on the home page, you need to set @books and @albums in the same way as the above @movies
 
