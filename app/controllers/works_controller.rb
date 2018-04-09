770f7ff4555c0eecb100ea90d51c25e5dca772fb
Lily Sky <lilyxsky@gmail.com>
2018-04-09 16:04:10 -0700

    homepage + all media pages linked in very basic form

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,22 +1,46 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
+    # where(category: "Book")
   end
 
   def show
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @work = Work.find_by(id: work_id)
   end
 
   def update
+    @work = Work.find_by(id: work_id)
+    if @work.update(work_params)
+      redirect_to works_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,11 @@
 Rails.application.routes.draw do
   get 'upvotes/index'
 
-  get 'upvotes/show'
-
-  get 'upvotes/new'
-
-  get 'upvotes/create'
-
-  get 'upvotes/edit'
-
-  get 'upvotes/update'
-
-  get 'upvotes/destroy'
-
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
+  resources :works
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root 'works#index'
+  root 'upvotes#index'
 end
