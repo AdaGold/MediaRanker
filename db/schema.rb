25973bd58d137fae18eb8306de4cdc06e0af1de4
Lily Sky <lilyxsky@gmail.com>
2018-04-09 15:33:11 -0700

    add_reference migration + model relationships connected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409215530) do
+ActiveRecord::Schema.define(version: 20180409222845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20180409215530) do
     t.date "vote_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_upvotes_on_user_id"
+    t.index ["work_id"], name: "index_upvotes_on_work_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +42,6 @@ ActiveRecord::Schema.define(version: 20180409215530) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "upvotes", "users"
+  add_foreign_key "upvotes", "works"
 end
