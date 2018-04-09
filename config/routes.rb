99235ac447b05b93a27244a858b2b4fcb0b69515
Lily Sky <lilyxsky@gmail.com>
2018-04-09 15:18:15 -0700

    destroy + regenerate controllers with methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,51 @@
 Rails.application.routes.draw do
+  get 'upvotes/index'
+
+  get 'upvotes/show'
+
+  get 'upvotes/new'
+
+  get 'upvotes/create'
+
+  get 'upvotes/edit'
+
+  get 'upvotes/update'
+
+  get 'upvotes/destroy'
+
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :works
+  # resources :users
+  # resources :upvotes
+
+  root 'works#index'
 end
--- test/controllers/upvotes_controller_test.rb
+++ test/controllers/upvotes_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe UpvotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index" do
+    get upvotes_index_url
+    value(response).must_be :success?
+  end
+
+  it "should get show" do
+    get upvotes_show_url
+    value(response).must_be :success?
+  end
+
+  it "should get new" do
+    get upvotes_new_url
+    value(response).must_be :success?
+  end
+
+  it "should get create" do
+    get upvotes_create_url
+    value(response).must_be :success?
+  end
+
+  it "should get edit" do
+    get upvotes_edit_url
+    value(response).must_be :success?
+  end
+
+  it "should get update" do
+    get upvotes_update_url
+    value(response).must_be :success?
+  end
+
+  it "should get destroy" do
+    get upvotes_destroy_url
+    value(response).must_be :success?
+  end
+
 end
