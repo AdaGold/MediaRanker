ae7fda70d5a904b4cd8348e80e2975913e385de6
Lily Sky <lilyxsky@gmail.com>
2018-04-09 22:54:19 -0700

    most voted works on homepage, but precarious

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,6 +3,16 @@ class UpvotesController < ApplicationController
     @books = Work.where(category: "Book").limit(2)
     @albums = Work.where(category: "Album").limit(2)
     @movies = Work.where(category: "Movie").limit(2)
+
+    # @upvotes = Upvote.all
+    # Upvote.group(:work_id).order('count_id DESC').limit(5).count(:id)
+
+    tallied = Upvote.group(:work_id).count
+    top = tallied.select {|k,v| v == tallied.values.max }
+
+    @featured = Work.find_by(id: top.keys)
+    @count = top.values
+
   end
 
   def show
@@ -22,4 +32,6 @@ class UpvotesController < ApplicationController
 
   def destroy
   end
+
+  private
 end
