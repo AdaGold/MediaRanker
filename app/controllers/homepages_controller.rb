0439fea7b29c9fc516d56fded527de080ea27ceb
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-17 13:41:34 -0700

    removed comments

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -8,20 +8,6 @@ class HomepagesController < ApplicationController
     @movies = Work.top_ten("movie")
 
     # find the one with highest votes
-    @spotlight = Work.spotlight
-    
-    # # go to each category and get the highest Work
-    # highest_book = @books.first
-    # highest_album = @albums.first
-    # highest_movie = @movies.first
-    # # compare each category to find the one with the most votes
-    # @spotlight = [highest_book, highest_album, highest_movie].compact.max_by {|work| work.votes.length}
-    # # keep it in a variable to display in view.
+    @spotlight = Work.spotlight 
   end
-
-
-
-
-
-
 end
