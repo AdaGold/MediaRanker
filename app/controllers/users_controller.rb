c202fa95a5601384573153af63cb781edc5e85bb
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-18 12:55:25 -0700

    Created login actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def login
-    username = params[:user][:username]
+    username = params[:user][:name]
 
-    found_user = User.find_by(username: name)
+    found_user = User.find_by(name: username)
 
     if found_user
       # We DID find a user!
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       flash[:message] = "Logged in as returning user!"
     else
       # We did not find an existing user. Let's try to make one!
-      new_user = User.new(username: username)
+      new_user = User.new(name: username)
       new_user.save
       # TODO: What happens if saving fails?
       session[:user_id] = new_user.id
@@ -31,11 +31,11 @@ class UsersController < ApplicationController
     return redirect_to root_path
   end
 
-  def current
-    @user = User.find_by(id: session[:user_id])
-    if @user.nil?
-      head :not_found
-      return
-    end 
-  end
+  # def current
+  #   @user = User.find_by(id: session[:user_id])
+  #   if @user.nil?
+  #     head :not_found
+  #     return
+  #   end 
+  # end
 end
