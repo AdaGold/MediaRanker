0d895103d889b193171ec2a3eb0843aa55b3b8d0
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-18 14:12:11 -0700

    addes some validation tests to work

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -4,4 +4,23 @@ describe Work do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  describe "validations" do
+    it "can be valid" do
+      is_valid = works(:valid_work_all_fields).valid?
+      assert(is_valid )
+    end
+
+    it "is invalid if there is no title" do
+      work = works(:invalid_work_noTitle)
+
+      is_valid = work.valid?
+
+      refute( is_valid )
+    end
+  end
+
+
+
+
 end
+ 
